74a26871de34790ea1782d5ed2a3b724b4bf480a
Valentina Mora <vmora19@bu.edu>
2025-03-29 21:38:12 -0400

    completed initial creation of short url generator using local host

--- code.js
+++ code.js
@@ -1,11 +1,82 @@
-const express = require("express");
+const express = require("express"); //express module from Node.js
 
-const app = express();
+const mysql = require("mysql2"); //mysql2 library
 
-app.use(express.static("public"));
+const app = express(); //application instance of express
 
-app.get("/", function(request, response){
-    response.sendFile(__dirname + "/public/index.html");
+app.use(express.static("public")); //make express instance connect to public folder for access
+app.use(express.json()); //handles JSON requests
+
+const con = mysql.createConnection({//create connection from mysql2 library to database
+    host:"localhost",
+    user:"root",
+    database:"shorturls"
 });
 
-app.listen(3000);
+con.connect(function(error){
+    if(error){ //if there was error in the connection
+        console.log("Database connection failed", error);
+    }
+    else{
+        console.log("Database connected succesfully");
+    }
+})
+
+app.get("/", function(request, response){ //GET method request to page
+    response.sendFile(__dirname + "/public/index.html"); //displays home page
+});
+
+app.post("/api/create-short-url", function(request, response){ //POST method to create a short url
+    let uniqueID = Math.random().toString(36).replace(/[^a-z0-9]/gi, '').substring(2,10); //creaate the uniqueID for a shortened link
+    let sql = `INSERT INTO links(longurl, shorturlid) VALUES('${request.body.longurl}', '${uniqueID}')`; //add the new shortened link to our db
+    con.query(sql, function(error, result){ //run the query
+        if(error){//if there was an error
+            response.status(500).json({
+                status:"notok",
+                message:"could not add to database"
+            });
+        }
+        else{
+            response.status(200).json({
+                status:"ok",
+                shorturlid: uniqueID
+            });
+        }
+    })
+});
+
+app.get("/api/get-all-short-urls", function(request, response){ //GET method to get all short urls
+    let sql = `SELECT * FROM links`; //get all the rows from links table
+    con.query(sql, function(error, result){ //run the query
+        if(error){ //if there was an error
+            response.status(500).json({
+                status:"notok",
+                message:"could not get all short urls"
+            }); 
+        }
+        else{
+            response.status(200).json(result); //returns status ok and shorturlid
+        }
+    })
+});
+
+app.get("/:shorturlid", function(request, response){//GET method for links with shorturlID
+    let shorturlid = request.params.shorturlid; //extract shorturlID
+    let sql = `SELECT * FROM links WHERE shorturlid = '${shorturlid}' LIMIT 1`; //select rows that match our shorturlID
+    con.query(sql, function(error, result){//run the query
+        if(error){//if there was an error
+            response.status(500).json({
+                status:"notok",
+                message:"something went wrong with retrieving the shorturlID"
+            }); 
+        }
+        if (result.length === 0) { //if there were not any rows that matched our shourturlID
+            response.status(404).json({ 
+                status: "notok", 
+                message: "Short URL not found" });
+            return;
+        }
+        response.redirect(result[0].longurl); //else redirect to page using longurl
+    })
+})
+app.listen(3000);//listener for our local host
---
+++ public/script.js
@@ -0,0 +1,61 @@
+const host = "http://localhost:3000/"; //host
+
+        document.querySelector("#create-short-url").addEventListener("click", function(){
+        let longurl = document.querySelector("#input-long-url").value.trim();
+        if(longurl.length == 0){ //if the user did not input anything
+            alert("Enter valid URL"); //display error
+            return;
+        }
+        else if(!(longurl.startsWith("http://") || longurl.startsWith("https://"))){ //if the user input link but it is not valid
+            alert("Enter valid link"); //display error
+            return;
+        }
+        fetch(host + "api/create-short-url", {
+            method: "POST", //posting our long url to server
+            body: JSON.stringify({ //send as JSON string
+                longurl:longurl
+            }),
+            headers:{
+                "Content-type": "application/json; charset = UTF-8" //content we are sending has json data
+            }
+        }).then(function(response){
+            return response.json(); //return response
+        }).then(function(data){ //check what was in the response
+            if(data.status == "ok"){ //if the request was succesful
+                //display the new link on page
+                document.querySelector("p").innerText = host + data.shorturlid;
+                document.querySelector("a").href = host + data.shorturlid;
+
+                //add the link to our history table
+                let html = `
+                <tr>
+                    <td>${longurl}</td>
+                    <td>${host}${data.shorturlid}</td>
+                </tr>
+                `;
+                document.querySelector("table tbody").innerHTML += html;
+            }
+        }).catch(function(error){ //catch errors
+            alert("Something went wrong");
+        })
+    });
+    (function(){
+        fetch(host + "api/get-all-short-urls").then(function(response){ //get method for all shortened urls
+            return response.json(); //get response
+        }).then(function(data){ //check the data
+
+            //get all the rows of history table
+            let html = "";
+            for(let i = 0; i < data.length; i++){
+                html += `
+                <tr>
+                    <td>${data[i].longurl}</td>
+                    <td>${host}${data[i].shorturlid}</td>
+                </tr>
+                `;
+            }
+            document.querySelector("table tbody").innerHTML = html; //display results
+        }).catch(function(error){ //catch errors
+            alert("Something went wrong");
+        })
+    })();
